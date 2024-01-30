a3d2207a13d7315656177758d4014a5a74992ebf
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-01-30 12:32:10 -0400

    created components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import ContactList from "./components/ContactList";
+function App() {
+    return (
+        <>
+            <ContactList />
+        </>
+    );
+}
+
+export default App;
